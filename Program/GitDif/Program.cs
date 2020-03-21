9b4716312e7222d073828fe1c2084614c329ec20
webibee <61194124+webibee@users.noreply.github.com>
2020-03-21 13:20:13 +0000

    Finished

---
+++ Program/GitDif/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace GitDif
+{
+    class Program
+    {
+        private static TextFiles User_Input_Class_Instantiation;
+        static void Main()
+        {
+            bool Leave_Loop = false;
+            string[] User_Input = new string[0];
+            List<char> First_Contents = new List<char>();
+            List<char> Second_Contents = new List<char>();
+            while (!Leave_Loop) //Error checking User's command and parameter inputs - only allowing 'dif' command and valid text file names beginning with "GitRepositories_" and being of type ".txt"
+            {
+                try
+                {
+                    Console.Write(">: [Input] ");
+                    User_Input = Console.ReadLine().Split();
+                    if ((InputCheck(User_Input[0], User_Input[1]) == true) & (InputCheck(User_Input[0], User_Input[2]) == true))
+                    {
+                        User_Input_Class_Instantiation = new TextFiles(User_Input);
+                        First_Contents = User_Input_Class_Instantiation.FirstIndividualCharacters();
+                        Second_Contents = User_Input_Class_Instantiation.SecondIndividualCharacters();
+                        Leave_Loop = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid command or file(s) entered...");
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            bool Text_Files_Different = false;
+            try
+            {
+                for (int Index = 0; Index < Second_Contents.Count(); Index++)// For loop cycling through each individual characters in both text files and comparing them
+                {
+                    if (First_Contents[Index] != Second_Contents[Index])
+                    {
+                        Console.Write($">: [Output] ");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write($"{User_Input[1]} and {User_Input[2]} are different");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Text_Files_Different = true;
+                        break;
+                    }
+                }
+                if (!Text_Files_Different)
+                {
+                    Console.Write($">: [Output] ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write($"{User_Input[1]} and {User_Input[2]} are not different");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"\n {e.Message}");
+            }
+        }
+        public static bool InputCheck(string Command, string File)
+        {
+            if (Command.Contains("dif"))
+            {
+                if ((File.Contains("GitRepositories_")) & (File.Substring(File.Length - 4) == ".txt")) return true; // Only returns true if text files are in format "GitRepositories_*.txt"
+                else return false;
+            }
+            else return false;
+        }
+    }
+}
---
+++ Program/GitDif/TextFile.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace GitDif
+{
+    class TextFiles
+    {
+        public static string FirstFileName;
+        public static string SecondFileName;
+        public TextFiles(string[] f_name) //Class constructor - setting the file names to class-dependent properties/"names"
+        {
+            FirstFileName = f_name[1];
+            SecondFileName = f_name[2];
+        }
+        private List<string> FirstContentsList() // All of the first text file's contents (strings) into an string list
+        {
+            return new List<string>(File.ReadAllLines(FirstFileName.ToString()));
+        }
+        private List<string> SecondContentsList() // All of the second text file's contents (strings) into an string list
+        {
+            return new List<string>(File.ReadAllLines(SecondFileName.ToString()));
+        }
+        public List<char> FirstIndividualCharacters() // Splitting first text file's contents (strings) into individual characters and storing in new list(char) 
+        {
+            List<char> Temp_List = new List<char>();
+            List<string> Temp_First_Contents = FirstContentsList();
+            foreach(string str in Temp_First_Contents)
+            {
+                str.Split();
+                foreach(char chars in str)
+                {
+                    Temp_List.Add(chars);
+                }
+            }
+            return Temp_List;
+        }
+        public List<char> SecondIndividualCharacters() // Splitting second text file's contents (strings) into individual characters and storing in new list(char) 
+        {
+            List<char> Temp_List = new List<char>();
+            List<string> Temp_Second_Contents = SecondContentsList();
+            foreach (string str in Temp_Second_Contents)
+            {
+                str.Split();
+                foreach (char chars in str)
+                {
+                    Temp_List.Add(chars);
+                }
+            }
+            return Temp_List;
+        }
+    }
+}
